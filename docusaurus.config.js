8d29dfe0f4bf72dad5f06ee88c39bbdadea23cdb
QianMoo0121 <98728586+QianMoo0121@users.noreply.github.com>
2025-03-01 13:14:23 +0800

    增加搜索功能

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -115,4 +115,25 @@ const config = {
     }),
 };
 
-module.exports = config;
+// In your `docusaurus.config.js`:
+module.exports = {
+  // ... Your other configurations.
+  themes: [
+    // ... Your other themes.
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+      ({
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+
+        // For Docs using Chinese, it is recomended to set:
+        language: ["en", "zh"],
+
+        // If you're using `noIndex: true`, set `forceIgnoreNoIndex` to enable local index:
+        // forceIgnoreNoIndex: true,
+      }),
+    ],
+  ],
+};
