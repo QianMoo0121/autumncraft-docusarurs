24b7485da1fbdfc1ad53b278dd78fe9848980d5a
QianMoo0121 <98728586+QianMoo0121@users.noreply.github.com>
2025-03-01 13:40:34 +0800

    增加搜索功能
    awa

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,9 +24,8 @@ const config = {
   // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'zh-Hans',
-    locales: ['zh_Hans'], 
+    locales: ['zh-Hans'],
   },
-
   presets: [
     [
       'classic',
@@ -131,7 +130,7 @@ module.exports = {
         docsRouteBasePath: ['/docs'],  // 文档路径（与项目配置一致）
         blogRouteBasePath: ['/blog'],  // 博客路径
         // 语言设置（支持多语言）
-        language: ['zh'],        // 包含中文需安装nodejieba
+        language: ['zh_Hans'],        // 包含中文需安装nodejieba
         // 增强配置
         hashed: true,         // 启用哈希缓存（推荐）
         highlightSearchTermsOnTargetPage: true, // 高亮搜索结果关键词
